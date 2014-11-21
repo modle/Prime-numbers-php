56a7c9b324699bea6481e775e33d8ccbf3f4bb6b
Matthew Odle <odle.matthew@gmail.com>
2014-11-21 12:52:24 -0600

    Update prime_limited.php
    Bug fixes: numbers less than 1, or max less than min cause exit

--- prime_limited.php
+++ prime_limited.php
@@ -6,13 +6,29 @@
     <?php
         $minNum=$_REQUEST['min_number'];
         $maxNum=$_REQUEST['max_number'];
-        print("The prime numbers in your range are: <br/>");
         $count=0;
         
         if($maxNum>20000)
         {
             exit("Are you trying to make your browser cry? Try a number smaller than 20,000 so we don't all die of boredom");
         }
+        
+        if($maxNum<1)
+        {
+            exit("Both numbers must be greater than 0");
+        }
+                
+        if($minNum<1)
+        {
+            exit("Both numbers must be greater than 0");
+        }
+        
+        if($minNum>$maxNum)
+        {
+            exit("The starting number must be less than the ending number");
+        }
+        
+        print("The prime numbers in your range are: <br/>");
         
         for($y=$minNum; $y<=$maxNum; $y++)
         {
