93411f1d91404dcc52ada9a78a6a84a54dd6d97c
Matthew Odle <odle.matthew@gmail.com>
2014-11-22 21:04:29 -0600

    Update prime.php
    Moved from prime_limited: maximum, negative fix, min greater than max fix
    Added: file saving

--- prime.php
+++ prime.php
@@ -7,8 +7,33 @@
     <?php
         $minNum=$_REQUEST['min_number'];
         $maxNum=$_REQUEST['max_number'];
-        print("The prime numbers in your range are: <br/>");
+        
+        $fileName = "primes.txt";
+        $fp=fopen($fileName, 'w');
+        
         $count=0;
+        if($maxNum>20000)
+        
+        {
+            exit("Are you trying to make your browser cry? Try a number smaller than 20,000 so we don't all die of boredom");
+        }
+
+        if($maxNum<1)
+        {
+            exit("Both numbers must be greater than 0");
+        }
+
+        if($minNum<1)
+        {
+            exit("Both numbers must be greater than 0");
+        }
+
+        if($minNum>=$maxNum)
+        {
+            exit("The starting number must be less than the ending number");
+        }
+        
+        print("The prime numbers in your range are: <br/>");
         
         for($y=$minNum; $y<=$maxNum; $y++)
         {
@@ -41,16 +66,15 @@
             if($prime==true)
             {
                 print($y . "<br/>"); //break separated
-                //print($y . ", "); //comma separated
                 $count=$count+1;
-            } else
-            {
-                //print($num . " is NOT a prime number <br/>");
-            }
+                fwrite($fp, $y . ", ");
+            } 
         }
         
-        print("There are " . $count . " prime numbers in your range <br/>");
-        
+        print("There are " . $count . " prime numbers in the range: " . $minNum . " to " . $maxNum . "<br/>");
+        print("Results saved --> primes.txt");
+        fwrite($fp, "\n There are " . $count . " prime numbers in the range: " . $minNum . " to " . $maxNum);
+        fclose($fp);
     ?>
 </body>
 </html>
